0a3937bf4a78f14e0ead33973b4a5c810f05dc71
diosamuel <koding12sam@gmail.com>
2026-06-25 22:21:20 +0700

    fix: index ingest

---
+++ index.py
@@ -0,0 +1,14 @@
+import json
+import os
+import time
+
+from bruin.ingest import ingest
+
+JSONL_PATH = os.path.join(os.path.dirname(__file__), "metrics.jsonl")
+
+while True:
+    line = ingest()
+    with open(JSONL_PATH, "a") as f:
+        f.write(line + "\n")
+    print(line)
+    time.sleep(1)
